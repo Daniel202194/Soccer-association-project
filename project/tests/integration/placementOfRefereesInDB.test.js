ff84fc7131fed195c5f2e8abfef44052d2697063
Nicole Dezoraive <nicoled@post.bgu.ac.il>
2021-06-08 16:58:37 +0300

    /

--- project/tests/integration/placementOfRefereesInDB.test.js
+++ project/tests/integration/placementOfRefereesInDB.test.js
@@ -1,5 +1,8 @@
 const { addRefereesToMatch } = require('../../routes/utils/representive_manager_utils.js');
+const { getRFA } = require('../../routes/utils/representive_manager_utils.js');
 
+
+//to check again
 describe('Adding a referee to a game at DB', () => {
     test('The referee was added successfully', async () =>
     {
@@ -27,29 +30,11 @@ describe('Adding a referee to a game at DB', () => {
     
 });
 
-describe('Adding a referee to a game at DB', () => {
-    test('The referee was added successfully', async () =>
+describe('Check that RFA subscribes to the system', () => {
+    test('successfully, RFA is in the system', async () =>
     {
-        const result = await addRefereesToMatch(10, 11, 12, 87);
-        expect(result).toBe(3);
-    });
-    
-    test('Failure, the referee was not added', async () =>
-    {
-        const result = await addRefereesToMatch(10, 11, 11, 87);
-        expect(result).not.toBe(3);
-        const result = await addRefereesToMatch(10, 12, 12, 87);
-        expect(result).not.toBe(3);
-        const result = await addRefereesToMatch(12, 12, 12, 87);
-        expect(result).not.toBe(3);
-        const result = await addRefereesToMatch(11, 12, 12, 87);
-        expect(result).not.toBe(3);
-        const result = await addRefereesToMatch(10, 10, 12, 87);
-        expect(result).not.toBe(3);
-        const result = await addRefereesToMatch(10, 10, 12, 1);
-        expect(result).not.toBe(3);
-        const result = await addRefereesToMatch(13, 14, 15, 71);
-        expect(result).not.toBe(3);
+        const result = await getRFA();
+        expect(result).toEqual({"username": "daniel"});
     });
     
 });
